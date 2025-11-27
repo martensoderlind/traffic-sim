efd25432d799b87c1705b16b4fddaf2d67c4a101
mårten söderlind <marten.soderlind@gmail.com>
2025-11-27 17:11:07 +0100

    added some more roads to test functionality

--- cmd/sim/main.go
+++ cmd/sim/main.go
@@ -12,20 +12,32 @@ func main(){
 
 
 	n1:= &road.Node{ID:"n1",X:0,Y:0}
-	n2:= &road.Node{ID:"n1",X:100,Y:0}
+	n2:= &road.Node{ID:"n2",X:100,Y:0}
+	n3:= &road.Node{ID:"n3",X:100,Y:100}
 
-	r1 :=road.NewRoad("r1",n1,n2,20)
+	r1 :=road.NewRoad("r1",n1,n2,40)
+	r2 :=road.NewRoad("r2",n2,n3,40)
+	r3 :=road.NewRoad("r3",n3,n2,40)
+	r4 :=road.NewRoad("r4",n2,n1,40)
 
 	v1 := &vehicle.Vehicle{
 		ID:"car1",
 		Road: r1,
 		Position: 0,
-		Speed: 10,
-		MaxSpeed:15,
+		Speed: 20,
+		MaxSpeed:25,
 	}
 
-	world := sim.NewWorld([]*road.Road{r1},[]*road.Node{n1,n2},[]*vehicle.Vehicle{v1})
-	simulator := sim.NewSimulation(world, 100*time.Millisecond)
+	world := sim.NewWorld([]*road.Road{r1,r2,r3,r4},[]*road.Node{n1,n2,n3},[]*vehicle.Vehicle{v1})
+	simulator := sim.NewSimulator(world, 100*time.Millisecond)
+
+	// intersections := sim.BuildIntersections([]*road.Road{r1,r2,r3,r4}, []*road.Node{n1, n2,n3})
+	// fmt.Println("Intersections:", len(intersections))
+	// for  _,ints:=range intersections{
+	// 	fmt.Println("intersection connections:",ints.ID)
+	// 	fmt.Println("intersection incoming:",ints.Incoming)
+	// 	fmt.Println("intersection outgoing:",ints.Outgoing)
+	// }
 
 	fmt.Println("starting simulation..")
 	go simulator.Start()
