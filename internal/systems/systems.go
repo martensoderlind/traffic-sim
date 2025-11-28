51a9a98c423af92b280653b744d8294f49dffec6
mårten söderlind <marten.soderlind@gmail.com>
2025-11-28 16:16:44 +0100

    moved sub system pathfinding system to new file

--- internal/systems/systems.go
+++ internal/systems/systems.go
@@ -1,9 +1,6 @@
 package systems
 
 import (
-	"math/rand"
-	"traffic-sim/internal/road"
-	"traffic-sim/internal/vehicle"
 	"traffic-sim/internal/world"
 )
 
@@ -31,46 +28,3 @@ func (sm *SystemManager) Update(w *world.World, dt float64) {
 	}
 }
 
-type PathfindingSystem struct{}
-
-func NewPathfindingSystem() *PathfindingSystem {
-	return &PathfindingSystem{}
-}
-
-func (ps *PathfindingSystem) Update(w *world.World, dt float64) {
-	w.Mu.Lock()
-	defer w.Mu.Unlock()
-
-	for _, v := range w.Vehicles {
-		if v.Distance >= v.Road.Length && v.Speed > 0 {
-			nextRoad := ps.findNextRoad(w, v)
-			
-			if nextRoad != nil {
-				v.Road = nextRoad
-				v.Distance = 0
-			} else {
-				v.Speed = 0
-			}
-		}
-	}
-}
-
-func (ps *PathfindingSystem) findNextRoad(w *world.World, v *vehicle.Vehicle) *road.Road {
-	intersection := w.IntersectionsByNode[v.Road.To.ID]
-	if intersection == nil || len(intersection.Outgoing) == 0 {
-		return nil
-	}
-
-	available := make([]*road.Road, 0, len(intersection.Outgoing))
-	for _, r := range intersection.Outgoing {
-		if r.ID != v.Road.ID {
-			available = append(available, r)
-		}
-	}
-
-	if len(available) == 0 {
-		return nil
-	}
-
-	return available[rand.Intn(len(available))]
-}
