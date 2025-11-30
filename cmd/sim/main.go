e3f7ee96e3253d19069cef70ce5be5731be90ab1
mårten söderlind <marten.soderlind@gmail.com>
2025-11-30 11:47:06 +0100

    removed default roads

--- cmd/sim/main.go
+++ cmd/sim/main.go
@@ -41,45 +41,11 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 }
 
 func main() {
-	n1 := &road.Node{ID: "n1", X: 100, Y: 300}
-	n2 := &road.Node{ID: "n2", X: 400, Y: 300}
-	n3 := &road.Node{ID: "n3", X: 400, Y: 100}
-	n4 := &road.Node{ID: "n4", X: 700, Y: 100}
-
-	r1 := road.NewRoad("r1", n1, n2, 40)
-	r2 := road.NewRoad("r2", n2, n3, 40)
-	r3 := road.NewRoad("r3", n3, n4, 40)
-	r4 := road.NewRoad("r4", n3, n2, 40)
-	r5 := road.NewRoad("r5", n2, n1, 40)
-
-	v1 := &vehicle.Vehicle{
-		ID:       "car1",
-		Road:     r1,
-		Distance: 0,
-		Pos:      vehicle.Vec2{X: r1.From.X, Y: r1.From.Y},
-		Speed:    30,
-	}
-
-	v2 := &vehicle.Vehicle{
-		ID:       "car2",
-		Road:     r2,
-		Distance: 0,
-		Pos:      vehicle.Vec2{X: r2.From.X, Y: r2.From.Y},
-		Speed:    25,
-	}
-
-	v3 := &vehicle.Vehicle{
-		ID:       "car3",
-		Road:     r5,
-		Distance: 50,
-		Pos:      vehicle.Vec2{X: r5.From.X, Y: r5.From.Y},
-		Speed:    35,
-	}
-
+	
 	world := world.New(
-		[]*road.Road{r1, r2, r3, r4, r5},
-		[]*road.Node{n1, n2, n3, n4},
-		[]*vehicle.Vehicle{v1, v2, v3},
+		[]*road.Road{},      
+		[]*road.Node{},      
+		[]*vehicle.Vehicle{},
 	)
 
 	simulator := sim.NewSimulator(world, 16*time.Millisecond)
