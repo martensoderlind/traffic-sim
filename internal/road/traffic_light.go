8afe2de45bd9df3475277ab68f8886bbeebce1c3
mårten söderlind <marten.soderlind@gmail.com>
2025-12-02 12:22:45 +0100

    setting up struct for traffic lights

---
+++ internal/road/traffic_light.go
@@ -0,0 +1,77 @@
+package road
+
+type LightState int
+
+const (
+	LightRed LightState = iota
+	LightYellow
+	LightGreen
+)
+
+type TrafficLight struct {
+	ID           string
+	Intersection *Intersection
+	ControlledRoads []*Road
+	State        LightState
+	Timer        float64
+	GreenTime    float64
+	YellowTime   float64
+	RedTime      float64
+	Enabled      bool
+}
+
+func NewTrafficLight(id string, intersection *Intersection) *TrafficLight {
+	return &TrafficLight{
+		ID:           id,
+		Intersection: intersection,
+		ControlledRoads: make([]*Road, 0),
+		State:        LightRed,
+		Timer:        0.0,
+		GreenTime:    10.0,
+		YellowTime:   2.0,
+		RedTime:      8.0,
+		Enabled:      true,
+	}
+}
+
+func (tl *TrafficLight) AddControlledRoad(r *Road) {
+	tl.ControlledRoads = append(tl.ControlledRoads, r)
+}
+
+func (tl *TrafficLight) Update(dt float64) {
+	if !tl.Enabled {
+		return
+	}
+
+	tl.Timer += dt
+
+	switch tl.State {
+	case LightGreen:
+		if tl.Timer >= tl.GreenTime {
+			tl.State = LightYellow
+			tl.Timer = 0.0
+		}
+	case LightYellow:
+		if tl.Timer >= tl.YellowTime {
+			tl.State = LightRed
+			tl.Timer = 0.0
+		}
+	case LightRed:
+		if tl.Timer >= tl.RedTime {
+			tl.State = LightGreen
+			tl.Timer = 0.0
+		}
+	}
+}
+
+func (tl *TrafficLight) CanProceed() bool {
+	return tl.State == LightGreen || !tl.Enabled
+}
+
+func (tl *TrafficLight) ShouldSlow() bool {
+	return tl.State == LightYellow && tl.Enabled
+}
+
+func (tl *TrafficLight) IsRed() bool {
+	return tl.State == LightRed && tl.Enabled
+}
