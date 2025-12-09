c586a2a92c293b7656483ea315d6069a660bbfd6
mårten söderlind <marten.soderlind@gmail.com>
2025-12-09 13:18:25 +0100

    updated rendering of vehicles and roads

--- internal/renderer/road_renderer.go
+++ internal/renderer/road_renderer.go
@@ -9,10 +9,14 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
-type RoadRenderer struct{}
+type RoadRenderer struct{
+	shadowOffset float64
+}
 
 func NewRoadRenderer() *RoadRenderer {
-	return &RoadRenderer{}
+	return &RoadRenderer{
+		shadowOffset:  3.0,
+	}
 }
 
 func (rr *RoadRenderer) RenderRoads(screen *ebiten.Image, roads []*road.Road) {
@@ -22,10 +26,8 @@ func (rr *RoadRenderer) RenderRoads(screen *ebiten.Image, roads []*road.Road) {
 }
 
 func (rr *RoadRenderer) renderSingleRoad(screen *ebiten.Image, rd *road.Road) {
-	x1 := rd.From.X
-	y1 := rd.From.Y
-	x2 := rd.To.X
-	y2 := rd.To.Y
+	x1, y1 := rd.From.X, rd.From.Y
+	x2, y2 := rd.To.X, rd.To.Y
 
 	dx := x2 - x1
 	dy := y2 - y1
@@ -43,55 +45,135 @@ func (rr *RoadRenderer) renderSingleRoad(screen *ebiten.Image, rd *road.Road) {
 		offset = rd.Width * 0.5
 	}
 
-	halfWidth := rd.Width / 2.0
-
 	x1 += perpX * offset
 	y1 += perpY * offset
 	x2 += perpX * offset
 	y2 += perpY * offset
 
-	p1x := float32(x1 + perpX*halfWidth)
-	p1y := float32(y1 + perpY*halfWidth)
-
-	p2x := float32(x1 - perpX*halfWidth)
-	p2y := float32(y1 - perpY*halfWidth)
-
-	p3x := float32(x2 - perpX*halfWidth)
-	p3y := float32(y2 - perpY*halfWidth)
-
-	p4x := float32(x2 + perpX*halfWidth)
-	p4y := float32(y2 + perpY*halfWidth)
-
-	vertices := []ebiten.Vertex{
-		{DstX: p1x, DstY: p1y, SrcX: 0, SrcY: 0, ColorR: 0.31, ColorG: 0.31, ColorB: 0.35, ColorA: 1},
-		{DstX: p2x, DstY: p2y, SrcX: 0, SrcY: 0, ColorR: 0.31, ColorG: 0.31, ColorB: 0.35, ColorA: 1},
-		{DstX: p3x, DstY: p3y, SrcX: 0, SrcY: 0, ColorR: 0.31, ColorG: 0.31, ColorB: 0.35, ColorA: 1},
-		{DstX: p4x, DstY: p4y, SrcX: 0, SrcY: 0, ColorR: 0.31, ColorG: 0.31, ColorB: 0.35, ColorA: 1},
-	}
-
-	indices := []uint16{0, 1, 2, 0, 2, 3}
-
-	screen.DrawTriangles(vertices, indices, emptyImage(screen), nil)
+	rr.drawRoadShadow(screen, x1, y1, x2, y2, rd.Width, perpX, perpY)
+	rr.drawRoadBase(screen, x1, y1, x2, y2, rd.Width, perpX, perpY)
+	// rr.drawRoadMarkings(screen, x1, y1, x2, y2, rd.Width, rd.Length, perpX, perpY)
+	rr.drawRoadEdges(screen, x1, y1, x2, y2, rd.Width, perpX, perpY)
 }
 
 func (rr *RoadRenderer) RenderNodes(screen *ebiten.Image, nodes []*road.Node) {
 	for _, node := range nodes {
 		x := float32(node.X)
 		y := float32(node.Y)
-
-		vector.FillCircle(
-			screen,
-			x, y,
-			8,
-			color.RGBA{100, 100, 120, 255},
-			false,
-		)
+		
+		vector.FillCircle(screen, x+1, y+1, 8, color.RGBA{0, 0, 0, 80}, false)
+		
+		vector.FillCircle(screen, x, y, 8, color.RGBA{80, 80, 90, 255}, false)
+		
+		vector.FillCircle(screen, x, y, 8, color.RGBA{45, 45, 50, 255}, false)
+		
+		vector.StrokeCircle(screen, x, y, 8, 1.5, color.RGBA{234, 231, 228, 255}, false)
 	}
 }
 
-func emptyImage(screen *ebiten.Image) *ebiten.Image {
-	const size = 1
-	img := ebiten.NewImage(size, size)
+
+func (rr *RoadRenderer) drawRoadShadow(screen *ebiten.Image, x1, y1, x2, y2, width, perpX, perpY float64) {
+	halfWidth := width / 2.0
+	
+	shadowX := float32(rr.shadowOffset * perpX)
+	shadowY := float32(rr.shadowOffset * perpY)
+	
+	p1x := float32(x1 + perpX*halfWidth) + shadowX
+	p1y := float32(y1 + perpY*halfWidth) + shadowY
+	p2x := float32(x1 - perpX*halfWidth) + shadowX
+	p2y := float32(y1 - perpY*halfWidth) + shadowY
+	p3x := float32(x2 - perpX*halfWidth) + shadowX
+	p3y := float32(y2 - perpY*halfWidth) + shadowY
+	p4x := float32(x2 + perpX*halfWidth) + shadowX
+	p4y := float32(y2 + perpY*halfWidth) + shadowY
+
+	vertices := []ebiten.Vertex{
+		{DstX: p1x, DstY: p1y, SrcX: 0, SrcY: 0, ColorR: 0, ColorG: 0, ColorB: 0, ColorA: 0.3},
+		{DstX: p2x, DstY: p2y, SrcX: 0, SrcY: 0, ColorR: 0, ColorG: 0, ColorB: 0, ColorA: 0.3},
+		{DstX: p3x, DstY: p3y, SrcX: 0, SrcY: 0, ColorR: 0, ColorG: 0, ColorB: 0, ColorA: 0.3},
+		{DstX: p4x, DstY: p4y, SrcX: 0, SrcY: 0, ColorR: 0, ColorG: 0, ColorB: 0, ColorA: 0.3},
+	}
+
+	indices := []uint16{0, 1, 2, 0, 2, 3}
+	screen.DrawTriangles(vertices, indices, createWhiteImage(), nil)
+}
+
+func (rr *RoadRenderer) drawRoadBase(screen *ebiten.Image, x1, y1, x2, y2, width, perpX, perpY float64) {
+	halfWidth := width / 2.0
+	
+	p1x := float32(x1 + perpX*halfWidth)
+	p1y := float32(y1 + perpY*halfWidth)
+	p2x := float32(x1 - perpX*halfWidth)
+	p2y := float32(y1 - perpY*halfWidth)
+	p3x := float32(x2 - perpX*halfWidth)
+	p3y := float32(y2 - perpY*halfWidth)
+	p4x := float32(x2 + perpX*halfWidth)
+	p4y := float32(y2 + perpY*halfWidth)
+
+	baseColor := color.RGBA{45, 45, 50, 255}
+	
+	vertices := []ebiten.Vertex{
+		{DstX: p1x, DstY: p1y, SrcX: 0, SrcY: 0, ColorR: 0.18, ColorG: 0.18, ColorB: 0.2, ColorA: 1},
+		{DstX: p2x, DstY: p2y, SrcX: 0, SrcY: 0, ColorR: 0.16, ColorG: 0.16, ColorB: 0.18, ColorA: 1},
+		{DstX: p3x, DstY: p3y, SrcX: 0, SrcY: 0, ColorR: 0.16, ColorG: 0.16, ColorB: 0.18, ColorA: 1},
+		{DstX: p4x, DstY: p4y, SrcX: 0, SrcY: 0, ColorR: 0.18, ColorG: 0.18, ColorB: 0.2, ColorA: 1},
+	}
+
+	indices := []uint16{0, 1, 2, 0, 2, 3}
+	screen.DrawTriangles(vertices, indices, createWhiteImage(), nil)
+	
+	_ = baseColor
+}
+
+// func (rr *RoadRenderer) drawRoadMarkings(screen *ebiten.Image, x1, y1, x2, y2, width, length, perpX, perpY float64) {
+// 	dx := x2 - x1
+// 	dy := y2 - y1
+	
+// 	dashLength := 15.0
+// 	gapLength := 10.0
+// 	segmentLength := dashLength + gapLength
+// 	numSegments := int(length / segmentLength)
+	
+// 	for i := 0; i < numSegments; i++ {
+// 		t1 := float64(i) * segmentLength / length
+// 		t2 := (float64(i)*segmentLength + dashLength) / length
+		
+// 		if t2 > 1.0 {
+// 			t2 = 1.0
+// 		}
+		
+// 		mx1 := float32(x1 + dx*t1)
+// 		my1 := float32(y1 + dy*t1)
+// 		mx2 := float32(x1 + dx*t2)
+// 		my2 := float32(y1 + dy*t2)
+		
+// 		vector.StrokeLine(screen, mx1, my1, mx2, my2, 1.5,
+// 			color.RGBA{200, 200, 180, 255}, false)
+// 	}
+// }
+
+func (rr *RoadRenderer) drawRoadEdges(screen *ebiten.Image, x1, y1, x2, y2, width, perpX, perpY float64) {
+	halfWidth := width / 2.0
+	
+	edge1x1 := float32(x1 + perpX*halfWidth)
+	edge1y1 := float32(y1 + perpY*halfWidth)
+	edge1x2 := float32(x2 + perpX*halfWidth)
+	edge1y2 := float32(y2 + perpY*halfWidth)
+	
+	edge2x1 := float32(x1 - perpX*halfWidth)
+	edge2y1 := float32(y1 - perpY*halfWidth)
+	edge2x2 := float32(x2 - perpX*halfWidth)
+	edge2y2 := float32(y2 - perpY*halfWidth)
+	
+	edgeColor := color.RGBA{234, 231, 228, 125}
+	
+	vector.StrokeLine(screen, edge1x1, edge1y1, edge1x2, edge1y2, 1.5, edgeColor, false)
+	vector.StrokeLine(screen, edge2x1, edge2y1, edge2x2, edge2y2, 1.5, edgeColor, false)
+}
+
+
+func createWhiteImage() *ebiten.Image {
+	img := ebiten.NewImage(1, 1)
 	img.Fill(color.White)
 	return img
 }
--- internal/renderer/vehicle_renderer.go
+++ internal/renderer/vehicle_renderer.go
@@ -9,10 +9,14 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
-type VehicleRenderer struct{}
+type VehicleRenderer struct{
+	shadowOffset float32
+}
 
 func NewVehicleRenderer() *VehicleRenderer {
-	return &VehicleRenderer{}
+	return &VehicleRenderer{
+		shadowOffset: 2.0,
+	}
 }
 
 func (vr *VehicleRenderer) RenderVehicles(screen *ebiten.Image, vehicles []*vehicle.Vehicle) {
@@ -45,24 +49,100 @@ func (vr *VehicleRenderer) renderSingleVehicle(screen *ebiten.Image, v *vehicle.
 	}
 
 	rotated := make([][2]float32, 4)
+	shadow := make([][2]float32, 4)
+	
 	for i, corner := range corners {
 		rotated[i][0] = cx + corner[0]*cos - corner[1]*sin
 		rotated[i][1] = cy + corner[0]*sin + corner[1]*cos
+		
+		shadow[i][0] = rotated[i][0] + vr.shadowOffset
+		shadow[i][1] = rotated[i][1] + vr.shadowOffset
 	}
 
-	vertices := []ebiten.Vertex{
-		{DstX: rotated[0][0], DstY: rotated[0][1], SrcX: 0, SrcY: 0, ColorR: 1.0, ColorG: 0.2, ColorB: 0.2, ColorA: 1},
-		{DstX: rotated[1][0], DstY: rotated[1][1], SrcX: 0, SrcY: 0, ColorR: 1.0, ColorG: 0.2, ColorB: 0.2, ColorA: 1},
-		{DstX: rotated[2][0], DstY: rotated[2][1], SrcX: 0, SrcY: 0, ColorR: 1.0, ColorG: 0.2, ColorB: 0.2, ColorA: 1},
-		{DstX: rotated[3][0], DstY: rotated[3][1], SrcX: 0, SrcY: 0, ColorR: 1.0, ColorG: 0.2, ColorB: 0.2, ColorA: 1},
+	shadowVerts := []ebiten.Vertex{
+		{DstX: shadow[0][0], DstY: shadow[0][1], SrcX: 0, SrcY: 0, ColorR: 0, ColorG: 0, ColorB: 0, ColorA: 0.4},
+		{DstX: shadow[1][0], DstY: shadow[1][1], SrcX: 0, SrcY: 0, ColorR: 0, ColorG: 0, ColorB: 0, ColorA: 0.4},
+		{DstX: shadow[2][0], DstY: shadow[2][1], SrcX: 0, SrcY: 0, ColorR: 0, ColorG: 0, ColorB: 0, ColorA: 0.4},
+		{DstX: shadow[3][0], DstY: shadow[3][1], SrcX: 0, SrcY: 0, ColorR: 0, ColorG: 0, ColorB: 0, ColorA: 0.4},
 	}
-
 	indices := []uint16{0, 1, 2, 0, 2, 3}
+	screen.DrawTriangles(shadowVerts, indices, vr.createWhiteImage(), nil)
 
-	screen.DrawTriangles(vertices, indices, emptyImage(screen), nil)
+	bodyVerts := []ebiten.Vertex{
+		{DstX: rotated[0][0], DstY: rotated[0][1], SrcX: 0, SrcY: 0, ColorR: 0.9, ColorG: 0.25, ColorB: 0.25, ColorA: 1},
+		{DstX: rotated[1][0], DstY: rotated[1][1], SrcX: 0, SrcY: 0, ColorR: 0.9, ColorG: 0.25, ColorB: 0.25, ColorA: 1},
+		{DstX: rotated[2][0], DstY: rotated[2][1], SrcX: 0, SrcY: 0, ColorR: 0.7, ColorG: 0.15, ColorB: 0.15, ColorA: 1},
+		{DstX: rotated[3][0], DstY: rotated[3][1], SrcX: 0, SrcY: 0, ColorR: 0.7, ColorG: 0.15, ColorB: 0.15, ColorA: 1},
+	}
+	screen.DrawTriangles(bodyVerts, indices, vr.createWhiteImage(), nil)
 
-	vector.StrokeLine(screen, rotated[0][0], rotated[0][1], rotated[1][0], rotated[1][1], 1, color.RGBA{255, 150, 150, 255}, false)
-	vector.StrokeLine(screen, rotated[1][0], rotated[1][1], rotated[2][0], rotated[2][1], 1, color.RGBA{255, 150, 150, 255}, false)
-	vector.StrokeLine(screen, rotated[2][0], rotated[2][1], rotated[3][0], rotated[3][1], 1, color.RGBA{255, 150, 150, 255}, false)
-	vector.StrokeLine(screen, rotated[3][0], rotated[3][1], rotated[0][0], rotated[0][1], 1, color.RGBA{255, 150, 150, 255}, false)
+	windshieldH := height * 0.25
+	windshield := [][2]float32{
+		{-hw * 0.7, -hh},
+		{hw * 0.7, -hh},
+		{hw * 0.5, -hh + windshieldH},
+		{-hw * 0.5, -hh + windshieldH},
+	}
+	
+	windRotated := make([][2]float32, 4)
+	for i, corner := range windshield {
+		windRotated[i][0] = cx + corner[0]*cos - corner[1]*sin
+		windRotated[i][1] = cy + corner[0]*sin + corner[1]*cos
+	}
+
+	windVerts := []ebiten.Vertex{
+		{DstX: windRotated[0][0], DstY: windRotated[0][1], SrcX: 0, SrcY: 0, ColorR: 0.4, ColorG: 0.5, ColorB: 0.6, ColorA: 0.8},
+		{DstX: windRotated[1][0], DstY: windRotated[1][1], SrcX: 0, SrcY: 0, ColorR: 0.4, ColorG: 0.5, ColorB: 0.6, ColorA: 0.8},
+		{DstX: windRotated[2][0], DstY: windRotated[2][1], SrcX: 0, SrcY: 0, ColorR: 0.3, ColorG: 0.4, ColorB: 0.5, ColorA: 0.6},
+		{DstX: windRotated[3][0], DstY: windRotated[3][1], SrcX: 0, SrcY: 0, ColorR: 0.3, ColorG: 0.4, ColorB: 0.5, ColorA: 0.6},
+	}
+	screen.DrawTriangles(windVerts, indices, createWhiteImage(), nil)
+
+	headlightOffset := hh * 0.8
+	headlightSize := float32(1.5)
+	
+	leftHeadlight := [2]float32{
+		cx + (-hw*0.5)*cos - (-headlightOffset)*sin,
+		cy + (-hw*0.5)*sin + (-headlightOffset)*cos,
+	}
+	rightHeadlight := [2]float32{
+		cx + (hw*0.5)*cos - (-headlightOffset)*sin,
+		cy + (hw*0.5)*sin + (-headlightOffset)*cos,
+	}
+
+	vector.FillCircle(screen, leftHeadlight[0], leftHeadlight[1], headlightSize, 
+		color.RGBA{255, 255, 200, 255}, false)
+	vector.FillCircle(screen, rightHeadlight[0], rightHeadlight[1], headlightSize, 
+		color.RGBA{255, 255, 200, 255}, false)
+
+	if v.Speed < 1.0 {
+		tailOffset := hh * 0.9
+		tailSize := float32(1.2)
+		
+		leftTail := [2]float32{
+			cx + (-hw*0.6)*cos - (tailOffset)*sin,
+			cy + (-hw*0.6)*sin + (tailOffset)*cos,
+		}
+		rightTail := [2]float32{
+			cx + (hw*0.6)*cos - (tailOffset)*sin,
+			cy + (hw*0.6)*sin + (tailOffset)*cos,
+		}
+
+		vector.FillCircle(screen, leftTail[0], leftTail[1], tailSize, 
+			color.RGBA{255, 50, 50, 255}, false)
+		vector.FillCircle(screen, rightTail[0], rightTail[1], tailSize, 
+			color.RGBA{255, 50, 50, 255}, false)
+	}
+
+	edgeColor := color.RGBA{180, 50, 50, 255}
+	vector.StrokeLine(screen, rotated[0][0], rotated[0][1], rotated[1][0], rotated[1][1], 1, edgeColor, false)
+	vector.StrokeLine(screen, rotated[1][0], rotated[1][1], rotated[2][0], rotated[2][1], 1, edgeColor, false)
+	vector.StrokeLine(screen, rotated[2][0], rotated[2][1], rotated[3][0], rotated[3][1], 1, edgeColor, false)
+	vector.StrokeLine(screen, rotated[3][0], rotated[3][1], rotated[0][0], rotated[0][1], 1, edgeColor, false)
+}
+
+func (vr *VehicleRenderer) createWhiteImage() *ebiten.Image {
+	img := ebiten.NewImage(1, 1)
+	img.Fill(color.White)
+	return img
 }
