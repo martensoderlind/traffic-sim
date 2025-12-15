82413eae360408cd0cb711313c8a04215883aa6e
mårten söderlind <marten.soderlind@gmail.com>
2025-12-15 16:33:09 +0100

    added more events and types

--- internal/events/events.go
+++ internal/events/events.go
@@ -1,18 +1,53 @@
 package events
 
+import "traffic-sim/internal/road"
+
 const (
-    EventRoadCreated = "road.created"
-    EventWorldLoaded  = "world.loaded"
-    EventSpawnPointCreated = "spawnpoint.created"
+	EventRoadCreated          = "road.created"
+	EventRoadDeleted          = "road.deleted"
+	EventNodeCreated          = "node.created"
+	EventNodeDeleted          = "node.deleted"
+	EventWorldLoaded          = "world.loaded"
+	EventSpawnPointCreated    = "spawnpoint.created"
+	EventDespawnPointCreated  = "despawnpoint.created"
+	EventTrafficLightCreated  = "trafficlight.created"
+	EventRoadPropertiesUpdated = "road.properties.updated"
 )
 
 type RoadCreatedEvent struct {
-    Road any
+	Road *road.Road
 }
+
+type RoadDeletedEvent struct {
+	RoadID string
+}
+
+type NodeCreatedEvent struct {
+	Node *road.Node
+}
+
+type NodeDeletedEvent struct {
+	NodeID string
+}
+
 type SpawnPointCreatedEvent struct {
-    SpawnPoint any
+	SpawnPoint *road.SpawnPoint
+}
+
+type DespawnPointCreatedEvent struct {
+	DespawnPoint *road.DespawnPoint
+}
+
+type TrafficLightCreatedEvent struct {
+	TrafficLight *road.TrafficLight
+}
+
+type RoadPropertiesUpdatedEvent struct {
+	Road     *road.Road
+	MaxSpeed float64
+	Width    float64
 }
 
 type WorldLoadedEvent struct {
-    World any
+	World any
 }
